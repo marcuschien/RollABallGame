42b800dfde82a7b27bff44a3f2ef78656148568d
marcuschien <mchien5@uwo.ca>
2017-11-06 16:14:50 -0500

    Add files via upload

---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelloWorld : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        print("\n Hello World!"); // This line is now ignored.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("\n Hello World!");
+    }
+}
